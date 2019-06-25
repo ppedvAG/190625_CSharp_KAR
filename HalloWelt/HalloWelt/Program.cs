27f4983704dbf7a6954b69a75c1585e918696471
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-25 11:45:35 +0200

    Strings in der Konsole ausgeben

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -29,7 +29,19 @@ namespace HalloWelt
             //string text = "Demo";
             #endregion
 
+            int zahl1 = 5;
+            int zahl2 = 10;
 
+            int ergebnis = zahl1 + zahl2;
+
+            // Variante 1:
+            Console.WriteLine("Die Summe von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+            // Variante 2;
+            // cw + TAB + TAB
+            Console.WriteLine("Die Summe von {0} und {1} ist {2}",zahl1,zahl2,ergebnis);
+            // Variante 3:
+            Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist {ergebnis}");
+            Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist {zahl1 + zahl2}");
 
             Console.ReadKey(); // Alternative: ReadLine();
         }
