bb72eb454d049c776c85ec44dcc6474727c35710
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-25 12:15:31 +0200

    Konvertieren

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -29,19 +29,54 @@ namespace HalloWelt
             //string text = "Demo";
             #endregion
 
-            int zahl1 = 5;
-            int zahl2 = 10;
+            #region Ausgabe von Strings
+            //int zahl1 = 5;
+            //int zahl2 = 10;
 
-            int ergebnis = zahl1 + zahl2;
+            //int ergebnis = zahl1 + zahl2;
 
-            // Variante 1:
-            Console.WriteLine("Die Summe von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
-            // Variante 2;
-            // cw + TAB + TAB
-            Console.WriteLine("Die Summe von {0} und {1} ist {2}",zahl1,zahl2,ergebnis);
-            // Variante 3:
-            Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist {ergebnis}");
-            Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist {zahl1 + zahl2}");
+            //// Variante 1:
+            //Console.WriteLine("Die Summe von " + zahl1 + " und " + zahl2 + " ist " + ergebnis);
+            //// Variante 2;
+            //// cw + TAB + TAB
+            //Console.WriteLine("Die Summe von {0} und {1} ist {2}",zahl1,zahl2,ergebnis);
+            //// Variante 3:
+            //Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist {ergebnis}");
+            //Console.WriteLine($"Die Summe von {zahl1} und {zahl2} ist {zahl1 + zahl2}"); 
+            #endregion
+
+            #region Konvertieren
+            //string eingabe = Console.ReadLine(); // eine ganze Zeile einlesen
+
+            //// Variante 1:
+            //int zahl1 = int.Parse(eingabe);
+            //// Variante 2:
+            //int zahl2 = Convert.ToInt32(eingabe);
+
+            //Console.WriteLine(zahl1 + zahl2);
+
+            //// Implizites Konvertieren:
+            //int z1 = 42;            // Int32 Wertebereich
+            //long großeZahl = 5;     // Int64 Wertebereich
+
+            //großeZahl = z1; // alles, was technisch gesehen in Int32 reinpasst, passt auch in Int64 rein
+
+            //großeZahl = 2;
+            //// z1 = großeZahl; // Kompiler-Fehler: Es "könnte" was größeres als Int32 drinnen stehen
+
+            //// Explizit Konvertieren
+            //z1 = Convert.ToInt32(großeZahl);// oder int.Parse();
+            //// Cast
+            //z1 = (int)großeZahl; 
+            #endregion
+
+            #region Operatoren
+            //int zahl1 = 5;
+            //double zahl2 = 3;
+
+            //double ergebnis = zahl1 / zahl2;
+            //Console.WriteLine(ergebnis); 
+            #endregion
 
             Console.ReadKey(); // Alternative: ReadLine();
         }
