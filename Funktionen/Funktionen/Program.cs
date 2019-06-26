dcb149442eb1c456af48dcdcae945a00cbed696e
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-26 09:26:36 +0200

    Eine erste Funktion erstellt

--- Funktionen/Funktionen/Program.cs
+++ Funktionen/Funktionen/Program.cs
@@ -10,6 +10,32 @@ namespace Funktionen
     {
         static void Main(string[] args)
         {
+            // STRG + D => Duplizieren
+            // STRG + C => (Ohne Markierung) Ganze Zeile Kopieren
+            // STRG + V => (Ohne Markierung) Ganze Zeile einfügen
+            // STRG + X => (Ohne Markierung) Ganze Zeile ausschneiden
+            // ALT + Pfeiltaste => Codezeile rauf/runter verschieben
+
+            // Programmstart:
+            // F5 -> Start mit Debugger
+            // STRG + F5 -> Start ohne Debugger
+            // F10 und F11 -> Starte im Einzelschrittmodus
+            //   -> F10: Methoden überspringen
+            //   -> F11: In Methoden hineinspringen
+
+            MachEtwas();
+            MachEtwas();
+            MachEtwas();
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
+
+        public static void MachEtwas() // void => keine Rückgabe
+        {
+            Console.WriteLine("Ich mache etwas ...");
+        }
+
     }
 }
