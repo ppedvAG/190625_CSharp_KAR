dd2fca61c71ab243653de6a5fb94a4af65f78965
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-26 12:26:42 +0200

    Properties

--- OOP/OOP/Person.cs
+++ OOP/OOP/Person.cs
@@ -7,28 +7,85 @@ using System.Threading.Tasks;
 namespace OOP
 {
     /* Empfohlene Namenskonvention:
-     * Felder werden klein geschrieben
-     * 
-     */ 
+     * Felder werden klein geschrieben (alternativ auch mit voranstehendem _ )
+     * Properties und Methoden werden groß geschrieben
+     * Klassennamen werden groß geschrieben
+     */
 
     class Person // Klasse == "Bauplan"
     {
-        // Felder (immer private)
-        private string vorname;
-        private string nachname;
-        private byte alter;
-        private decimal kontostand;
+        #region Variante ohne Properties
+        //// Felder (immer private)
+        //private string vorname;
+        //private string nachname;
+        //private byte alter;
+        //private decimal kontostand;
 
-        public void SetVorname(string neuerVorname) // Validierung
+        //public void SetVorname(string neuerVorname) // Validierung
+        //{
+        //    if(neuerVorname.Length < 3)
+        //        Console.WriteLine("Vorname zu kurz");
+        //    else
+        //        vorname = neuerVorname;
+        //}
+        //public string GetVorname()
+        //{
+        //    return vorname;
+        //} 
+        #endregion
+
+        private string vorname; // wird klein geschrieben !
+        public string Vorname
         {
-            if(neuerVorname.Length < 3)
-                Console.WriteLine("Vorname zu kurz");
-            else
-                vorname = neuerVorname;
+            get // Lesezugriff
+            {
+                return vorname;
+            }
+            set // Schreibzugriff
+            {
+                // vorname = value; // value ist der neue Wert, der gesetzt wird
+
+                if (value.Length < 3)
+                    Console.WriteLine("Vorname zu kurz");
+                else
+                    vorname = value;
+            }
         }
-        public string GetVorname()
+
+        // propfull + TAB + TAB
+        private string nachname;
+
+        public string Nachname
         {
-            return vorname;
+            get { return nachname; }
+            set { nachname = value; }
         }
+
+        // Seit C# 7.0: Kurzschreibweise mit Lambda-Ausdruck
+        // public string Nachname
+        // {
+        //    get => nachname; 
+        //    set => nachname = value; 
+        // }
+
+        // Autoproperty:
+        // prop + TAB + TAB
+
+        private byte alter;
+        public byte Alter
+        {
+            get => alter;
+            set
+            {
+                if (value < 120)
+                    alter = value;
+                else
+                    Console.WriteLine("Man darf nicht älter als 120 werden");
+            }
+        } // macht der Compiler ein propfull daraus
+
+        public decimal Kontostand { get; private set; }
+
+
     }
 }
--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -13,13 +13,20 @@ namespace OOP
             Random generator = new Random();
             Person p1 = new Person();
 
-            p1.SetVorname("Ka");
-            Console.WriteLine(p1.GetVorname());
+            //p1.SetVorname("Ka");
+            //Console.WriteLine(p1.GetVorname());
 
-            p1.SetVorname("Kai");
-            Console.WriteLine(p1.GetVorname());
+            //p1.SetVorname("Kai");
+            //Console.WriteLine(p1.GetVorname());
 
-            
+            p1.Vorname = "Peter";
+            Console.WriteLine(p1.Vorname);
+
+            Console.WriteLine(p1.Kontostand);
+
+            p1.Alter = 10;
+
+            p1.Alter = 200;
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
