8ea3c5efcbde7171b91b77f393bd7ae9ab830df0
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-26 09:55:56 +0200

    Dokumentatinskommentar, Funktionen mit und ohne Rückgabe

--- Funktionen/Funktionen/Program.cs
+++ Funktionen/Funktionen/Program.cs
@@ -23,19 +23,62 @@ namespace Funktionen
             //   -> F10: Methoden überspringen
             //   -> F11: In Methoden hineinspringen
 
+            // Dokumentation erstellen:
+            // Einstellungen -> Build -> XML Documentation File - Option aktivieren
+
             MachEtwas();
             MachEtwas();
             MachEtwas();
 
+            MachEtwas("Hallo Welt");
+
+            GibMirEineZufallszahl(100); // rückgabe verfällt
+
+            int zufallsZahl = GibMirEineZufallszahl(0, 1000);
+            Console.WriteLine(zufallsZahl);
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
         }
+        // Einzeilig
+        /* 
+         * 
+         * Mehrzeilig
+         */
+        // Dokumentationskommentar: Über einer Klasse oder einer Methode /// schreiben
 
+        #region Funktionen
+        /// <summary>
+        /// Diese Methode macht etwas
+        /// </summary>
         public static void MachEtwas() // void => keine Rückgabe
         {
             Console.WriteLine("Ich mache etwas ...");
         }
 
+        /// <summary>
+        /// Diese Methode macht etwas mit einem String-Parameter
+        /// </summary>
+        /// <param name="text">Der Inhalt dieses Parameters wird in die Konsole geschrieben</param>
+        public static void MachEtwas(string text)
+        {
+            Console.WriteLine($"Ich mache etwas mit einem Text: {text} ...");
+        }
+        public static int GibMirEineZufallszahl()
+        {
+            Random generator = new Random();
+            return generator.Next(1, 10);
+        }
+        public static int GibMirEineZufallszahl(int maxValue)
+        {
+            Random generator = new Random();
+            return generator.Next(1, maxValue);
+        }
+        public static int GibMirEineZufallszahl(int minimum, int maximum)
+        {
+            Random generator = new Random();
+            return generator.Next(minimum, maximum);
+        }
+        #endregion
     }
 }
