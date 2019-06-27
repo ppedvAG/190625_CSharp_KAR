32fbb0f4aa81f287d07cc7959800ebb9c22c504b
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-27 12:08:04 +0200

    Alles mit einem Catch-Block abfangen

--- ExceptionDemo/ExceptionDemo/Program.cs
+++ ExceptionDemo/ExceptionDemo/Program.cs
@@ -10,6 +10,34 @@ namespace ExceptionDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie eine Zahl ein:");
+            string eingabe = Console.ReadLine();
+
+
+
+            // try + TAB + TAB
+            int zahl = 0;
+            try
+            {
+                zahl = Convert.ToInt32(eingabe);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Es gibt einen Fehler 😭");
+                // Console.WriteLine(ex.Message); // originale Fehlermeldung ausgeben
+                // throw; // Fehler weiterwerfen
+
+                return; // beende das programm wegen einem fehler
+            }
+            finally
+            {
+                Console.WriteLine("Ich werde immer ausgeführt, egal ob es eine Exception gibt oder nicht");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine($"Das Doppelte ist : {zahl * 2}");
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
