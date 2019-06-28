a4d26b72a6d48e613f5e1d52cf12a5ac93c33980
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-28 11:51:50 +0200

    Init Taschenrechner

---
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
